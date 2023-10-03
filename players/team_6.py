f32dd04a9a9de7c44304bc5a947abc71ac87f472
FungluiKoo <fenglei.gu@nyu.edu>
2023-10-03 19:46:42 -0400

    Update team_6.py

--- players/team_6.py
+++ players/team_6.py
@@ -5,6 +5,9 @@ import sys
 
 random.seed(2)
 
+HORIZON = 10 # how far we can look
+DANGER_ZONE = 0.5 # how close we can get to an obstacle/person
+
 @dataclass
 class Vector:
     x: float
@@ -21,7 +24,7 @@ class Player:
         self.tsp_path = tsp_path
         self.num_players = num_players
 
-        # moving direction. make sure norm of (vx, vy) is 1
+        # unit vector representing direction of movement
         self.dir = Vector(1, 0)
 
         # next lookup checkpoint
@@ -35,6 +38,14 @@ class Player:
 
     def __calc_dist(self, a: Vector, b: Vector) -> float:
         return math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
+
+    # returns normalized vector from a to b
+    def __normalize(self, source: Vector, dest: Vector) -> Vector:
+        dist = self.__calc_dist(source, dest)
+        return Vector((dest.x - source.x)/dist, (dest.y - source.y)/dist)
+
+    def __innerprod(self, a: Vector, b: Vector) -> float:
+        return a.x*b.x + a.y*b.y
 
     # returns (x,y) of next stall we wanna visit
     def __next_stall(self) -> Vector:
@@ -55,7 +66,26 @@ class Player:
     # simulator calls this function when it passes the lookup information
     # this function is called if the player returns 'lookup' as the action in the get_action function
     def pass_lookup_info(self, other_players, obstacles):
-        pass
+        # update self.dir and self.next_ckpt according to new info
+        
+        self.dir = self.__normalize(self.pos, self.__next_stall())
+        safe_go = (10-DANGER_ZONE)/2 # how far we can go safely without lookup
+
+        # adjust direction based on obstacles & walls
+
+        
+        # look at other players
+        for player in other_players:
+            p = Vector(player.pos_x, player.pos_y)
+            u = self.__normalize(self.pos, p)
+            cos_theta = self.__innerprod(self.dir, u)
+            if cos_theta > 0:
+                # player is in front of us
+                d = self.__calc_dist(self.pos, p)
+                # update safe_go
+                safe_go = min(safe_go, (d-DANGER_ZONE)/(2*cos_theta))
+        
+        self.next_ckpt = Vector(self.pos.x + safe_go*self.dir.x, self.pos.y + safe_go*self.dir.y)
 
     # simulator calls this function when the player encounters an obstacle
     def encounter_obstacle(self):
@@ -68,34 +98,11 @@ class Player:
         # return 'lookup' or 'move'
         
         self.pos = Vector(pos_x, pos_y) # update current position
-
-        return 'lookup' if self.__calc_dist(self.pos, self.next_ckpt) < self.epsilon else 'move'
+        
+        vab = Vector(self.next_ckpt.x - pos_x, self.next_ckpt.y - pos_y)
+        return 'lookup' if self.__innerprod(vab, self.dir) < self.epsilon else 'move'
     
     # simulator calls this function to get the next move from the player
     # this function is called if the player returns 'move' as the action in the get_action function
     def get_next_move(self):
-        ns = self.get_closest_stall()
-        if self.pos_x <= 0:
-            self.sign_x = 1
-            self.vx = random.random()
-            self.vy = math.sqrt(1 - self.vx**2)
-
-        if self.pos_y <= 0:
-            self.sign_y = 1
-            self.vx = random.random()
-            self.vy = math.sqrt(1 - self.vx**2)
-
-        if self.pos_x >= 100:
-            self.sign_x = -1
-            self.vx = random.random()
-            self.vy = math.sqrt(1 - self.vx**2)
-
-        if self.pos_y >= 100:
-            self.sign_y = -1
-            self.vx = random.random()
-            self.vy = math.sqrt(1 - self.vx**2)
-
-        new_pos_x = self.pos_x + self.sign_x * self.vx
-        new_pos_y = self.pos_y + self.sign_y * self.vy
-
-        return new_pos_x, new_pos_y
+        return self.pos.x + self.dir.x, self.pos.y + self.dir.y
