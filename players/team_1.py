4efbf34cfafa4271db3c7c9180e8764b8be1d0aa
edc2147 <edc2147@columbia.edu>
2023-10-04 11:23:54 -0400

    basic movement

--- players/team_1.py
+++ players/team_1.py
@@ -20,6 +20,8 @@ class Player:
         self.sign_x = 1
         self.sign_y = 1
 
+        self.goal_stall = None
+
     # simulator calls this function when the player collects an item from a stall
     def collect_item(self, stall_id):
         pass
@@ -48,6 +50,57 @@ class Player:
     # simulator calls this function to get the next move from the player
     # this function is called if the player returns 'move' as the action in the get_action function
     def get_next_move(self):
+
+        closest_stall = None
+        min_distance = float('inf')
+
+        # if empty, stand still
+        if not self.stalls_to_visit:
+            # get_action should be looking down at phone ('move'?)
+            return self.pos_x, self.pos_y
+
+        # if we have no current trajectory we're following
+        if self.goal_stall is None:
+            for stall in self.stalls_to_visit:
+                # distance given we only need to be 1m away from edge of stall to collect
+                dist = math.dist((self.pos_x, self.pos_y),(stall.x + 1, stall.y + 1))
+                if dist < min_distance:
+                    closest_stall = stall
+                    min_distance = dist
+
+            print(f'Goal stall is at {closest_stall.x}, {closest_stall.y} while I am at {self.pos_x}, {self.pos_y}')
+
+            self.goal_stall = closest_stall
+            dist_to_edge = min_distance
+            print(f'Min distance is {min_distance}')
+        else:
+            dist_to_edge = math.dist((self.pos_x, self.pos_y), (self.goal_stall.x + 1, self.goal_stall.y + 1))
+
+        # if we are 1m away from goal stall
+        if dist_to_edge < 1:
+            # self.collect_item(self.goal_stall.id)
+            print(f'Collected item from stall {self.goal_stall.id}')
+
+            # should be done in collect_item
+            self.stalls_to_visit.remove(self.goal_stall)
+            self.goal_stall = None
+            return self.pos_x, self.pos_y
+
+        if self.pos_x < self.goal_stall.x:
+            self.sign_x = 1
+        elif self.pos_x > self.goal_stall.x:
+            self.sign_x = -1
+        else:
+            self.sign_x = 0
+
+        if self.pos_y < self.goal_stall.y:
+            self.sign_y = 1
+        elif self.pos_y > self.goal_stall.y:
+            self.sign_y = -1
+        else:
+            self.sign_y = 0
+
+        # boundary bounce?
         if self.pos_x <= 0:
             self.sign_x = 1
             self.vx = random.random()
@@ -71,4 +124,5 @@ class Player:
         new_pos_x = self.pos_x + self.sign_x * self.vx
         new_pos_y = self.pos_y + self.sign_y * self.vy
 
+        print(f'Moving to {new_pos_x}, {new_pos_y}')
         return new_pos_x, new_pos_y
