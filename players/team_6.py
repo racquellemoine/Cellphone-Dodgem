88540ef6cd14bb0339c9c0d3359c87bc86695061
chloe2781 <chloe.nguyen.2708@gmail.com>
2023-10-04 12:52:19 -0400

    debugged and added encounter obstacles function

--- players/team_6.py
+++ players/team_6.py
@@ -20,9 +20,12 @@ class Player:
         self.color = color
         self.pos = Vector(initial_pos_x, initial_pos_y)
         self.stalls_to_visit = stalls_to_visit
+        self.stalls_visited = []
+        self.curr_stall = self.__next_stall()
         self.T_theta = T_theta
         self.tsp_path = tsp_path
         self.num_players = num_players
+        self.known_obstacles = []
 
         # unit vector representing direction of movement
         self.dir = Vector(1, 0)
@@ -42,6 +45,8 @@ class Player:
     # returns normalized vector from a to b
     def __normalize(self, source: Vector, dest: Vector) -> Vector:
         dist = self.__calc_dist(source, dest)
+        if dist == 0:
+            return Vector(0,0)
         return Vector((dest.x - source.x)/dist, (dest.y - source.y)/dist)
 
     def __innerprod(self, a: Vector, b: Vector) -> float:
@@ -49,34 +54,50 @@ class Player:
 
     # returns (x,y) of next stall we wanna visit
     def __next_stall(self) -> Vector:
-        min_distance =  sys.maxsize
+        min_distance = sys.maxsize
         curr_stall = None
         for stall in self.stalls_to_visit:
-            curr_dist = math.sqrt((stall.x - self.pos_x)**2 + \
-                                                   (stall.y - self.pos_y)**2)
-            if curr_dist < min_distance:
+            curr_dist = self.__calc_dist(self.pos, stall)
+            if curr_dist < min_distance and stall not in self.stalls_visited:
                 min_distance =  curr_dist
                 curr_stall = stall
+        if curr_stall is None:
+            return Vector(0,0)
+        self.stalls_visited.append(curr_stall)
         return curr_stall
 
     # simulator calls this function when the player collects an item from a stall
     def collect_item(self, stall_id):
-        pass
+        self.stalls_visited.append(self.curr_stall)
+        self.curr_stall = self.__next_stall()
 
     # simulator calls this function when it passes the lookup information
     # this function is called if the player returns 'lookup' as the action in the get_action function
     def pass_lookup_info(self, other_players, obstacles):
         # update self.dir and self.next_ckpt according to new info
         
-        self.dir = self.__normalize(self.pos, self.__next_stall())
+        self.dir = self.__normalize(self.pos, Vector(self.curr_stall.x, self.curr_stall.y))
         safe_go = (10-DANGER_ZONE)/2 # how far we can go safely without lookup
 
         # adjust direction based on obstacles & walls
+        for obstacle in obstacles:
+            if obstacle not in self.known_obstacles:
+                self.known_obstacles.append(Vector(obstacle[1], obstacle[2]))
+                # obstacle is a tuple of (x,y,radius)
 
-        
+        for obstacle in obstacles:
+            o = Vector(obstacle[0], obstacle[1])
+            u = self.__normalize(self.pos, o)
+            cos_theta = self.__innerprod(self.dir, u)
+            if cos_theta > 0:
+                # obstacle is in front of us
+                d = self.__calc_dist(self.pos, o)
+                # update safe_go
+                safe_go = min(safe_go, (d-DANGER_ZONE)/(2*cos_theta))
+
         # look at other players
         for player in other_players:
-            p = Vector(player.pos_x, player.pos_y)
+            p = Vector(player[1], player[2])
             u = self.__normalize(self.pos, p)
             cos_theta = self.__innerprod(self.dir, u)
             if cos_theta > 0:
@@ -89,9 +110,42 @@ class Player:
 
     # simulator calls this function when the player encounters an obstacle
     def encounter_obstacle(self):
-        self.dir = Vector(1,0) # arbitrary direction; needs to be changed
-        self.sign_x *= -1
-        self.sign_y *= -1
+        # Try moving in the opposite direction of the current dir vector
+        new_pos = Vector(self.pos.x - self.dir.x, self.pos.y - self.dir.y)
+        if new_pos not in self.known_obstacles:
+            self.pos = new_pos
+            self.next_ckpt = Vector(self.pos.x + self.dir.x, self.pos.y + self.dir.y)
+            return
+
+        # If the opposite direction is blocked, try moving in a perpendicular direction
+        if self.dir.x == 0:
+            # If the current dir vector is vertical, try moving horizontally
+            new_pos = Vector(self.pos.x + 1, self.pos.y)
+            if new_pos not in self.known_obstacles:
+                self.dir = Vector(1, 0)
+                self.pos = new_pos
+                self.next_ckpt = Vector(self.pos.x + self.dir.x, self.pos.y + self.dir.y)
+                return
+            new_pos = Vector(self.pos.x - 1, self.pos.y)
+            if new_pos not in self.known_obstacles:
+                self.dir = Vector(-1, 0)
+                self.pos = new_pos
+                self.next_ckpt = Vector(self.pos.x + self.dir.x, self.pos.y + self.dir.y)
+                return
+        else:
+            # If the current dir vector is horizontal, try moving vertically
+            new_pos = Vector(self.pos.x, self.pos.y + 1)
+            if new_pos not in self.known_obstacles:
+                self.dir = Vector(0, 1)
+                self.pos = new_pos
+                self.next_ckpt = Vector(self.pos.x + self.dir.x, self.pos.y + self.dir.y)
+                return
+            new_pos = Vector(self.pos.x, self.pos.y - 1)
+            if new_pos not in self.known_obstacles:
+                self.dir = Vector(0, -1)
+                self.pos = new_pos
+                self.next_ckpt = Vector(self.pos.x + self.dir.x, self.pos.y + self.dir.y)
+                return
 
     # simulator calls this function to get the action 'lookup' or 'move' from the player
     def get_action(self, pos_x, pos_y):
