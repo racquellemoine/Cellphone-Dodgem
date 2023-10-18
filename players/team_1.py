26fb5fcf969a03c88c0328d5166cec4d8da7ed0b
edc2147 <edc2147@columbia.edu>
2023-10-18 02:25:07 -0400

    endgame movement fix. movement debug in progress

--- players/team_1.py
+++ players/team_1.py
@@ -6,7 +6,7 @@ from datetime import timedelta, datetime
 
 random.seed(2)
 
-#Constants
+# Constants
 MAX_SECS = 1
 
 
@@ -21,14 +21,14 @@ class Player:
         self.T_theta = T_theta
         self.tsp_path = tsp_path
         self.num_players = num_players
-        self.end_game = False 
+        self.end_game = False
 
         self.vx = random.random()
         self.vy = math.sqrt(1 - self.vx ** 2)
         self.sign_x = 1
         self.sign_y = 1
 
-        #Added functionality 
+        # Added functionality
         self.distance_grid = []
         for i in range(len(stalls_to_visit) + 1):
             row = []
@@ -37,11 +37,11 @@ class Player:
 
             # Add the row to the list
             self.distance_grid.append(row)
-        
+
         self.queue = deque()
         self.set_tsp_path()
         self.set_queue()
-        
+
         self.obstacles_list = []
         self.other_players_dict = {}
         self.field_vision = []
@@ -50,7 +50,7 @@ class Player:
         self.YDIM = 100  # constants.vis_width
         self.DELTA = 10  # temp, bc we can only move 1m per turn?
         self.max_time = timedelta(seconds=MAX_SECS)
-        self.WIN_RADIUS = 2 # temp, but bc stall is 2x2 and we have 1m border around
+        self.WIN_RADIUS = 2  # temp, but bc stall is 2x2 and we have 1m border around
         self.LOOKUP_RADIUS = 10
         self.start_node = self.pos_x, self.pos_y
 
@@ -60,6 +60,7 @@ class Player:
         self.collided = False
 
         self.collision_counter = 0
+        self.backward_steps = 2
 
     # simulator calls this function when the player collects an item from a stall
 
@@ -84,14 +85,14 @@ class Player:
         if stall_id == self.queue[0].id:
             self.queue.popleft()
             self.goal_stall = None
-
-        for stall in self.queue:
-            if stall.id == stall_id:
-                print("Stall id: ", stall.id)
-                self.queue.remove(stall)
-                self.goal_stall = None
-                break
-
+        else:
+            # do this because what if we by accident collect something along the way?
+            # IF goal_stall.id == stall_id, then set None, else, we still need the curr traj
+            for stall in self.queue:
+                if stall.id == stall_id:
+                    print("Stall id: ", stall.id)
+                    self.queue.remove(stall)
+                    break
 
     def pass_lookup_info(self, other_players, obstacles):
         """
@@ -108,18 +109,17 @@ class Player:
             if obstacle not in self.obstacles_list:
                 self.obstacles_list.append(obstacle)
 
-        # need to do something with how we only want to record if people are around us
-        # otherwise, it doesn't matter, we should pop them out.
-
-        # call to update the list of players
-        # self._update_players_list()
-
-        #if the player ID matches one of the IDs in other_players_list, remove it and add the new once
-
-        # set the dict value based on the player ID
-        # if the player is already in the dict, reassign. works for both new and old players seen
+        # set the dict value based on the player IDs of ONLY players we currently see
+        ## UPDATE THE DICT ONLY BASED ON WHO WE SEE IN OUR FIELD OF VISION, i.e. THE ONLY THREATS
+        current_players = {}
         for player in other_players:
-            self.other_players_dict[player[0]] = (player[1], player[2])
+            player_id, x, y = player
+            current_players[player_id] = (x, y)
+        self.other_players_dict = current_players
+
+        #BEFORE:::
+        # for player in other_players:
+        #     self.other_players_dict[player[0]] = (player[1], player[2])
 
         return
 
@@ -149,28 +149,36 @@ class Player:
         self.collision_counter += 1
         self.vx = random.random()
         self.vy = math.sqrt(1 - self.vx ** 2)
-        #self.sign_x *= -1
-        #self.sign_y *= -1
+        self.sign_x *= -1
+        self.sign_y *= -1
 
-        # self.sign_x *= -1
-        # self.sign_y *= -1
-
+        #TODO: keep this here?
+        self._bounce_off_boundaries(self.pos_x, self.pos_y)
 
     # simulator calls this function to get the action 'lookup', 'move', or 'lookup move' from the player
     def get_action(self, pos_x, pos_y):
         # return 'lookup' or 'move'
-        #end game
+        # end game
 
         if len(self.queue) == 0 or self.T_theta <= 10:
-            if not self.end_game:
-                self.pos_x = pos_x - 2
-                self.pos_y = pos_y - 2
-                self.end_game = True
+            # if not self.end_game:
+            #     print("END GAME")
+            #     self.pos_x = pos_x - 2
+            #     self.pos_y = pos_y - 2
+            #     self.end_game = True
+            if self.end_game:
+                # Move backward for 'backward_steps' steps
+                if self.backward_steps > 0:
+                    #GO LEFT based on current velocity. Just switch x sign
+                    self.sign_x *= -1
+                    self.sign_y = 1
+                else:
+                    self.end_game = False
             return 'move'
-        
+
         self.pos_x = pos_x
         self.pos_y = pos_y
-        #lookup whole time during game
+        # lookup whole time during game
         return 'lookup move'
 
     def _normalize(self, vx, vy):
@@ -199,7 +207,7 @@ class Player:
             self.vx = random.random()
             self.vy = math.sqrt(1 - self.vx ** 2)
 
-     #########################################################
+    #########################################################
     #                  From dodgem_game.py                  #
     #########################################################
 
@@ -358,10 +366,9 @@ class Player:
         x = random.random()
         y = random.random()
 
-
         # bias: 5% of the time. So any generated val <= 0.05 should return goal
         # else: multiply the random val by the constraint of the grid to get valid coord
-        
+
         if (x <= 0.05):
             x = goal_x
         else:
@@ -372,7 +379,7 @@ class Player:
         else:
             y *= YDIM
 
-        print(x, y)
+        # print(x, y)
         return x, y
 
     def _extend(self, current_node, new_point, delta):
@@ -390,7 +397,7 @@ class Player:
             x, y (tuple) - the new point in space to move towards
         """
 
-        vx= new_point[0] - current_node[0]
+        vx = new_point[0] - current_node[0]
         vy = new_point[1] - current_node[1]
         self.sign_x = 1
         self.sign_y = 1
@@ -436,68 +443,11 @@ class Player:
             player_coords = other_players[player_id]
             if self._check_collision_obstacle(player_coords, new_point):
                 # APPENDING TUPLE IN EXPECTED FORMAT
-                # PLEASE CHANGE IF NEEDED FOR CLARITY
+                #TODO PLEASE CHANGE IF NEEDED FOR CLARITY
                 self.field_vision.append((player_id, player_coords[0], player_coords[1]))
                 return False
 
-
         return True
-
-    def rrt(self, goal_point, obstacles, other_players):
-        nodes = []
-        parents = {}
-        nodes.append(self.start_node)
-        collision_count = 0
-        iter_count = 0
-        rrt_path = []
-
-        """should it keep calling it and updating it as it goes, i.e, the current position is updated?"""
-        begin = datetime.utcnow()
-        while datetime.utcnow() - begin < self.max_time:
-
-            new_point = self._get_new_point(self.XDIM, self.YDIM, goal_point)
-            # print(f'getting new point {new_point}')
-
-            if self._is_collision_free(obstacles, other_players, new_point):
-
-                # print(f'new point {new_point} is collision free')
-
-                closest_node = self._nearest_node(nodes, new_point)
-                # extend towards new_point
-                new_node = self._extend(closest_node, new_point, self.DELTA)
-                # check extension is also collision free
-                if self._is_collision_free(obstacles, other_players, new_node):
-                    # print(f'extended node {new_node} is collision free')
-
-                    nodes.append(new_node)
-                    parents[new_node] = closest_node
-
-                    # print(f'extended. parents {parents}')
-
-                    if self._win_condition(new_node, goal_point):
-
-                        # print("RRT win condition reached")
-
-                        # backtrack to get path
-                        curr_node = new_node
-                        while curr_node != self.start_node:
-                            rrt_path.append(curr_node)
-                            curr_node = parents[curr_node]
-                        rrt_path.append(self.start_node)
-                        rrt_path.reverse()
-                        return rrt_path
-
-                else:
-                    collision_count += 1
-            else:
-                collision_count += 1
-
-            iter_count += 1
-
-        # print(f'iter {iter_count}')
-
-        return rrt_path
-
 
     #########################################################
 
@@ -510,10 +460,9 @@ class Player:
         based on high traffic.
         """
         if len(self.queue) > 0:
-            #don't pop until we collect it!
+            # don't pop until we collect it!
             self.goal_stall = self.queue[0]
-            self.emergency_exit()
-
+            # self.emergency_exit()
 
     #########################################################
 
@@ -539,13 +488,20 @@ class Player:
             # print(f'AFTER getting next node Q={self.queue}')
             # # print(f'Goal stall is at {self.goal_stall.x}, {self.goal_stall.y}')
 
-        #print("This is the goal ", self.goal_stall.id, self.goal_stall.x, self.goal_stall.y)
         if self.goal_stall:
-            if self.collision_counter >=3:
-                self.vx = random.random()
-                self.vy = math.sqrt(1 - self.vx ** 2)
-                #self.sign_x *= -1
-                #self.sign_y *= -1
+            print("This is the goal ", self.goal_stall.id)
+
+            if self.collision_counter >= 3:
+                print(f'collision counter {self.collision_counter}')
+
+                # RANDOM COLLISION TRAJECTORY ALREADY CALCULATED IN ENCOUNTER OBSTACLE FXN
+
+                # self.vx = random.random()
+                # self.vy = math.sqrt(1 - self.vx ** 2)
+                # self.sign_x *= -1
+                # self.sign_y *= -1
+
+                self._bounce_off_boundaries(self.pos_x, self.pos_y)
 
                 new_pos_x = self.pos_x + self.sign_x * self.vx
                 new_pos_y = self.pos_y + self.sign_y * self.vy
@@ -555,13 +511,11 @@ class Player:
 
             goal_point = self.goal_stall.x, self.goal_stall.y
 
-            # if not self.is_rrt_planning:
-
             if self._is_collision_free(self.obstacles_list, self.other_players_dict, goal_point):
                 vx = self.goal_stall.x - self.pos_x
                 vy = self.goal_stall.y - self.pos_y
-                #self.sign_x = 1
-                #self.sign_y = 1
+                self.sign_x = 1
+                self.sign_y = 1
                 self.vx, self.vy = self._normalize(vx, vy)
 
                 self._bounce_off_boundaries(self.pos_x, self.pos_y)
@@ -570,28 +524,27 @@ class Player:
                 new_pos_y = self.pos_y + self.sign_y * self.vy
 
                 # print(f'Moving to {new_pos_x}, {new_pos_y}')
-
                 print("No obstacles in the way, moving directly to goal stall")
                 return new_pos_x, new_pos_y
             else:
                 # print("Obstacle detected, planning with RRT")
 
                 self.rrt_tree = [(curr_pos_x, curr_pos_y)]  # Initialize the RRT tree with the start node
-              #  print("This is the start node ", self.start_node)
+                #  print("This is the start node ", self.start_node)
                 # explore until new point is collision free or until 1 second has passed
                 begin = datetime.utcnow()
                 while datetime.utcnow() - begin < self.max_time:
-                    print("This is ", datetime.utcnow(), begin, self.max_time)
+                    # print("This is ", datetime.utcnow(), begin, self.max_time)
                     new_point = self._get_new_point(self.XDIM, self.YDIM, goal_point)
                     if self._is_collision_free(self.obstacles_list, self.other_players_dict, new_point):
                         break
 
                 closest_node = self._nearest_node(self.rrt_tree, new_point)
-                print("This is the new node ", closest_node, " this is the tree ", self.rrt_tree)
+                # print("This is the new node ", closest_node, " this is the tree ", self.rrt_tree)
 
                 new_node = self._extend(closest_node, new_point, self.DELTA)
-               # print("This is the new node ", new_node)
-                
+                # print("This is the new node ", new_node)
+
                 if self._is_collision_free(self.obstacles_list, self.other_players_dict, new_node):
                     # print(f'extending to new node {new_node}')
                     # print(f'DISTANCE CURR-NEW NODE {math.dist(self.start_node, new_node)}')
@@ -599,18 +552,21 @@ class Player:
                     # self._bounce_off_boundaries(self.pos_x, self.pos_y)
 
                     new_pos_x, new_pos_y = new_node
-                   # print("This is x, y ", new_pos_x, new_pos_y)
+                    # print("This is x, y ", new_pos_x, new_pos_y)
+                    print("returning from RRT")
+
                     return new_pos_x, new_pos_y
                 else:
+                    # TODO: WHAT ELSE DO WE DO WHEN WE ACTUALLY JUST HAD A COLLISION? HOW DO WE MOVE?
                     print("Collision detected while extending RRT tree")
 
                     self.vx = random.random()
-                    print("This is the unit vector", self.vx)
+                    # print("This is the unit vector", self.vx)
                     self.vy = math.sqrt(1 - self.vx ** 2)
-                    #self.sign_x *= -1
-                    #self.sign_y *= -1
+                    self.sign_x *= -1
+                    self.sign_y *= -1
 
-                    # self._bounce_off_boundaries(self.pos_x, self.pos_y)
+                    self._bounce_off_boundaries(self.pos_x, self.pos_y)
 
                     new_pos_x = self.pos_x + self.sign_x * self.vx
                     new_pos_y = self.pos_y + self.sign_y * self.vy
@@ -619,22 +575,29 @@ class Player:
 
 
         elif self.goal_stall is None and len(self.queue) == 0:
-            self._bounce_off_boundaries(self.pos_x, self.pos_y)
 
-            print(f'No more stalls to visit, stand still {self.pos_x}, {self.pos_y}')
+            # endgame, step to the left of the stall
+            if not self.end_game and self.backward_steps > 0:
+                self.end_game = True
 
-            return self.pos_x, self.pos_y
+                # print("not yet end game amd backward steps > 0")
 
-        # TO DO: WHAT ELSE DO WE DO WHEN WE ACTUALLY JUST HAD A COLLISION? HOW DO WE MOVE?
-        elif self.collided:
-            print("collision from prev turn")
-            self.collided = False
+                return self.pos_x, self.pos_y
 
-            self._bounce_off_boundaries(self.pos_x, self.pos_y)
+            elif self.backward_steps == 0:
 
-            new_pos_x = self.pos_x + self.sign_x * self.vx
-            new_pos_y = self.pos_y + self.sign_y * self.vy
+                # print("end game and backward steps == 0")
 
-            print(f'Collision detected, moving to new {new_pos_x}, {new_pos_y}')
-            return new_pos_x, new_pos_y
+                return self.pos_x, self.pos_y
 
+
+            elif self.end_game and self.backward_steps > 0:
+                self.backward_steps -= 1
+
+                # print(f'No more stalls to visit, move out of way {self.backward_steps} steps left')
+
+                self._bounce_off_boundaries(self.pos_x, self.pos_y)
+                new_pos_x = self.pos_x + self.sign_x * self.vx
+                new_pos_y = self.pos_y + self.sign_y * self.vy
+
+                return new_pos_x, new_pos_y
