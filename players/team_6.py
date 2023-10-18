0e398c936e47606f2ed493ec2bcc037c2c48ec28
FungluiKoo <fenglei.gu@nyu.edu>
2023-10-17 22:06:14 -0400

    Update team_6.py

--- players/team_6.py
+++ players/team_6.py
@@ -301,7 +301,7 @@ class Player:
         self.t_since_lkp += 1
         self.pos = Vector(pos_x, pos_y) # update current position
         
-        if self.pos.dist2(self.prev_pos) < EPSILON or self.pos.dist2(self.preprev_pos) < 0.3: # if we are oscilating/not moving
+        if self.pos.dist2(self.prev_pos) < EPSILON or self.pos.dist2(self.preprev_pos) < 0.5: # if we are oscilating/not moving
             self.dir = self.__randunit()
             npos = self.pos + self.dir
             while any(npos.dist2stall(obstacle) <= 2*DANGER_ZONE for obstacle in self.obstacles_known) or any(npos.dist2(player) < 2*DANGER_ZONE+1 for player in self.players_cached):
